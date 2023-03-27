dab838b04ac3083ed134451ee1fcdb44ba820fc6
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-27 15:38:01 +0000

    Add placeholder styling for App.js and NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>DCMS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
