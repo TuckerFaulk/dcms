bfc7f831eac424e96b2bed89cc3b6c3ef6e11f91
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-28 13:04:22 +0000

    Add SignInForm.js, route link in app.js and create sign in form

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import { Link, useHistory } from "react-router-dom";
+
+function SignInForm() {
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <Row>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={` p-4 `}>
+          <h1>Sign In</h1>
+          
+          <Form onSubmit={handleSubmit}> 
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => 
+                <Alert variant="warning" key={idx}>{message}</Alert>
+            )}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => 
+                <Alert variant="warning" key={idx}>{message}</Alert>
+            )}
+
+            <Button
+              type="submit"
+            >
+              Sign in
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => 
+                <Alert variant="warning" key={idx} className="mt-3">{message}</Alert>
+            )}
+            
+          </Form>
+        </Container>
+        <Container className={`mt-3`}>
+          <Link to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+}
+
+export default SignInForm;
