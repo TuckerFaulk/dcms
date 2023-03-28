d3e2ab21a08a962a78aa5279a1d0820577396081
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-03-28 11:04:54 +0000

    Add React Router for navlink routing in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from "../styles/NavBar.module.css"
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>DCMS</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>DCMS</Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink to="/" exact activeClassName={styles.Active}>
               <i className="fas fa-home"></i> Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin" activeClassName={styles.Active}>
               <i className="fas fa-sign-in-alt"></i> Sign In
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup" activeClassName={styles.Active}>
               <i className="fas fa-user-plus"></i> Sign Up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
